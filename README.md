31539d79d21e2ad3497c8327f7c7415c052f00ef
paroxayte <36929366+paroxayte@users.noreply.github.com>
2020-01-22 14:34:36 -0600

    add functionallity for pretty printing tables, hyperlink haddock to source

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ PrettyPrint Haskell records generically!
 This library includes classes and functions designed to accumulate and format
 record field acessors and their corresponding values. Recursive types are
 expanded in formatting. For detailed documentation
-and example usage read the [docs](https://paroxayte.github.io/pprecord/Text-Record-PrettyPrint.html).
+and example usage read the [docs](https://paroxayte.github.io/pprecord/Text-PrettyPrint-Records.html).
+For advanced formatting, this library depends on Text.PrettyPrint.Boxes from the
+[boxes](https://hackage.haskell.org/package/boxes-0.1.5) package.
--- src/Text/PrettyPrint/Records.hs
+++ src/Text/PrettyPrint/Records.hs
@@ -3,12 +3,13 @@
 {-# LANGUAGE FlexibleInstances   #-}
 {-# LANGUAGE FlexibleContexts    #-}
 
-module Text.PrettyPrint.Records (FQuery, VQuery, VFmt, format, fields, values,
-    dfltFmt, simpleFmt, Formatter(..)) where
+module Text.PrettyPrint.Records (FQuery, VQuery, RFmt, format, formatUntil, fields, values,
+    dfltFmt, simpleFmt, tableFmt, Format(..), TableFmt(..), formatTable) where
 
 import GHC.Generics
-import Data.Data
-import Text.PrettyPrint.Boxes hiding ((<>))
+import Data.Typeable (cast, Typeable)
+import Text.PrettyPrint.Boxes ((<+>), vcat, text, Box(), left, hsep, top)
+import Data.List (transpose)
 
 --- FQuery -------------------------------------------------------------------
 
@@ -23,10 +24,6 @@ instance FQuery' U1 where
 
 instance FQuery c => FQuery' (Rec0 c) where
     fields' _ = []
-
-instance (FQuery' f, FQuery' g) => FQuery' (f :+: g) where
-    fields' (L1 x) = fields' x
-    fields' (R1 x) = fields' x
 
 instance (FQuery' f, FQuery' g) => FQuery' (f :*: g) where
     fields' (x :*: y) = fields' x <> fields' y
@@ -54,10 +51,6 @@ class VQuery' f where
 instance VQuery' f => VQuery' (M1 t c f) where
     values' (M1 x) = values' x
 
-instance (VQuery' f, VQuery' g) => VQuery' (f :+: g) where
-    values' (L1 x) = values' x
-    values' (R1 x) = values' x
-
 instance (VQuery' f, VQuery' g) => VQuery' (f :*: g) where
     values' (x :*: y) = values' x <> values' y
 
@@ -76,20 +69,20 @@ class VQuery a where
     default values :: (Generic a, VQuery' (Rep a)) => a -> [String]
     values = values' . from
 
--- VFmt ------------------------------------------------------------------------
+-- RFmt ------------------------------------------------------------------------
 
--- |A Formatter is responsible for converting a Record into a formatted Box.
+-- |A Format is responsible for converting a Record into a formatted Box.
 --
 -- @
 --    __Example:__
---        dfltFmt :: Formatter a
---        dfltFmt = Formatter
+--        dfltFmt :: Format a
+--        dfltFmt = Format
 --            { arg = text
 --            , label = \\a b -> text (a <> ":") <+> b
 --            , finally = vcat left }
 -- @
 --
-data Formatter a = Formatter
+data Format a = Format
         { -- | Converts Record fields to Boxes
           arg     :: String -> Box,
           -- | Merge accessor with field
@@ -98,77 +91,130 @@ data Formatter a = Formatter
           finally :: [Box]  -> Box
         }
 
-class VFmt' f where
-    fvalues' :: (Typeable a, VFmt a) => f p -> Formatter a -> [Box]
+class RFmt' f where
+    fvalues' :: (Typeable a, RFmt a) => Int -> f p -> Format a -> [Box]
 
--- |Reduce Record to Box given a Formatter
+-- |Reduce Record to Box given a Format
 --
 -- @
 --
 --   __Example:__
---      data List a = List { label :: String, val :: a, tail :: List a }
---          | Nil deriving (Generic, Show)
---      instance FQuery (List a)
---      instance (Show a, Typeable a) => VFmt (List a)
---      test = List "head" 3 $ List "mid" 4 $ List "tail" 5 $ Nil
+--       data List a = List { label :: String, val :: a, tail :: List a }
+--           | Nil deriving (Generic, Show)
+--       instance FQuery (List a)
+--       instance (Show a, Typeable a) => RFmt (List a)
+--       test = List "head" 3 $ List "mid" 4 $ List "tail" 5 $ Nil
 --
---      ghci> printBox $ format test dfltFmt
---      label: "head"
---      val: 3
---      tail: label: "mid"
---            val: 4
---            tail: label: "tail"
---                  val: 5
---                  tail: Nil
+--       ghci> printBox $ format test dfltFmt
+--       label: "head"
+--       val: 3
+--       tail: label: "mid"
+--             val: 4
+--             tail: label: "tail"
+--                   val: 5
+--                   tail: Nil
 -- @
 --
-class (Typeable a, FQuery a, Show a) => VFmt a where
-    fvalues :: a -> Formatter a -> [Box]
-    default fvalues :: (Generic a, VFmt' (Rep a)) => a -> Formatter a -> [Box]
-    fvalues a = fvalues' (from a)
-    format :: a -> Formatter a -> Box
-    format a f = let
-        lhs = fields a
-        rhs = fvalues a f
-        in  if null rhs
-            then arg f . show $ a
-            else finally f $ zipWith (label f) lhs rhs
+class (Typeable a, FQuery a, Show a) => RFmt a where
+    fvalues :: Int -> a -> Format a -> [Box]
+    default fvalues :: (Generic a, RFmt' (Rep a)) => Int -> a -> Format a -> [Box]
+    fvalues 0 _ _ = [text "....."]
+    fvalues n a f = fvalues' n (from a) f
 
-instance VFmt' f => VFmt' (M1 t c f) where
-    fvalues' (M1 x) = fvalues' x
+instance RFmt' f => RFmt' (M1 t c f) where
+    fvalues' n (M1 x) = fvalues' n x
 
-instance (VFmt' f, VFmt' g) => VFmt' (f :+: g) where
-    fvalues' (L1 x) = fvalues' x
-    fvalues' (R1 x) = fvalues' x
-
-instance (VFmt' f, VFmt' g) => VFmt' (f :*: g) where
-    fvalues' (x :*: y) f = let
-        l = fvalues' x f
-        r = fvalues' y f
+instance (RFmt' f, RFmt' g) => RFmt' (f :*: g) where
+    fvalues' n (x :*: y) f = let
+        l = fvalues' n x f
+        r = fvalues' n y f
         in l <> r
 
-instance (Typeable c, Show c) => VFmt' (Rec0 c) where
-    fvalues' K1{unK1=v} f = let
-        g :: Typeable k => Formatter k -> Maybe k
+instance (Typeable c, Show c) => RFmt' (Rec0 c) where
+    fvalues' n K1{unK1=v} f = let
+        g :: Typeable k => Format k -> Maybe k
         g _ = cast v
         in case g f of
             Nothing -> [arg f . show $ v]
-            Just x  -> [format x f]
+            Just x  -> [formatUntil (n - 1) x f]
 
-instance VFmt' U1 where
-    fvalues' _ _ = []
+instance RFmt' U1 where
+    fvalues' _ _ _ = []
 
-instance VFmt' V1 where
-    fvalues' _ _ = []
+instance RFmt' V1 where
+    fvalues' _ _ _ = []
 
 -- Helpers ---------------------------------------------------------------------
 
-dfltFmt :: Formatter a
-dfltFmt = Formatter
+data TableFmt = TableFmt
+    { -- | Converts Record fields to Boxes
+      argT     :: String -> Box,
+      -- | Merge accessor with field
+      labelT :: String -> [Box] -> Box,
+      -- | Finally reduce list of boxes
+      finallyT :: [Box]  -> Box
+    }
+
+dfltFmt :: Format a
+dfltFmt = Format
     { arg = text
     , label = \a b -> text (a <> ":") <+> b
     , finally = vcat left }
 
+-- | A table formatter which produces a top-down table
+tableFmt :: TableFmt
+tableFmt = TableFmt
+    { argT = text
+    , labelT = \a bs -> vcat left $ text a : bs
+    , finallyT = hsep 1 top }
+
 -- | Given String representation of accessor and value return b
 simpleFmt :: (VQuery a, FQuery a) => (String -> String -> b) -> a  -> [b]
 simpleFmt f x = zipWith f (fields x) (values x)
+
+-- | Format Record a with up to n levels of recursivity
+formatUntil :: RFmt a => Int -> a -> Format a -> Box
+formatUntil n a f = let
+    lhs = fields a
+    rhs = fvalues n a f
+    in  if null rhs
+        then arg f . show $ a
+        else finally f $ zipWith (label f) lhs rhs
+
+-- | Format Record a record fully expanding the data structure if it is
+-- recursive
+format :: RFmt a => a -> Format a -> Box
+format = formatUntil (-1)
+
+-- | Format a list of RFmt instances as a table (no recursive expansion).
+--
+-- @
+--    __Example:__
+--        data Employee = E
+--           { eId :: Int
+--           , name :: String
+--           , email :: String } deriving (Generic, Show)
+--        instance FQuery Employee
+--        instance RFmt Employee
+--        e1 = E 3 \"John\" "johnbelcher@foobar.xyz"
+--        e1 = E 3 \"John\" "johnbelcher@foobar.xyz"
+--        e2 = E 17 \"Maria\" "Mariafoobar@net.net"
+--        e3 = E 1 \"Stanley\" "stanleytheceo@foobar.org"
+--        e4 = E 2 \"Kayla\" "klabar@foo.com"
+--        e5 = E 4 \"Sammy\" "sammersfoobar@foo.bar"
+--
+--        ghci> printBox $ formatTable [e1, e2, e3, e4, e5] tableFmt
+--        eId name      email
+--        3   \"John\"    "johnbelcher@foobar.xyz"
+--        17  \"Maria\"   "Mariafoobar@net.net"
+--        1   \"Stanley\" "stanleytheceo@foobar.org"
+--        2   \"Kayla\"   "klabar@foo.com"
+--        4   \"Sammy\"   "sammersfoobar@foo.bar"
+-- @
+--
+formatTable :: RFmt a => [a] -> TableFmt -> Box
+formatTable as t = let
+    minFmt = Format (argT t) undefined (finallyT t)
+    header = fields (head as)
+    fvals  = transpose $ flip (fvalues 1) minFmt <$> as
+    in finallyT t $ zipWith (labelT t) header fvals
